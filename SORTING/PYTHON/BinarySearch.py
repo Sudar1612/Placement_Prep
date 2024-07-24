3d1058a49210e763999fd9647567eae93d00650c
SUDARKODI S <sudarkodi1612@gmail.com>
2024-07-24 12:06:24 +0530

    Implemented binary search algorithm using RECURSIVE approach

--- SORTING/PYTHON/BinarySearch.py
+++ SORTING/PYTHON/BinarySearch.py
@@ -1,5 +1,5 @@
 # Time Complexity -> O(logn)
-def binarySearch(arr,search):
+def iterative(arr,search):
     begin ,end = 0, len(arr)-1
 
     while(begin <= end):
@@ -13,4 +13,20 @@ def binarySearch(arr,search):
 
     return None
 
-print(binarySearch( [2,4,5,6,7,8,9,10,12,13,14],8))
+def recursive(arr,search,begin,end):
+    if(begin <= end):
+        mid = (begin + end) //2
+        if(arr[mid] == search):
+            return mid
+        elif (arr[mid] < search):
+            recursive(arr,search,mid+1,end)
+        else:
+            recursive(arr,search,begin,mid-1)
+    else:
+        return None
+
+
+arr = [2,4,5,6,7,8,9,10,12,13,14]
+search = 19
+print(iterative( arr,search))
+print(recursive(arr,search,0,len(arr)-1))
