f845dea1f45adcc7c788edc40de77c14fb1c6ebd
SUDARKODI S <sudarkodi1612@gmail.com>
2024-07-24 11:43:55 +0530

    Implemented a simpler way for Selction sort algorihtm.

--- SORTING/PYTHON/Selection.py
+++ SORTING/PYTHON/Selection.py
@@ -12,8 +12,20 @@ def Selection(num_list,leng):
         num_list[ran-1],num_list[ind]=num_list[ind],num_list[ran-1]
         ran+=1
 
+# more simpler coding
+def selection_sort(arr):
+    for i in range(0,len(arr)-1):
+        mini = i
+        for j in range(i+1,len(arr)):
+            if(arr[j] < arr[mini]):
+                mini = j
+
+        if(mini != i):
+            arr[mini],arr[i] = arr[i],arr[mini]
+    return arr
 
 num_list=[5,15,3,12,17,0]
 print(num_list)
 Selection(num_list,len(num_list))
 print(num_list)
+print(selection_sort(num_list))
